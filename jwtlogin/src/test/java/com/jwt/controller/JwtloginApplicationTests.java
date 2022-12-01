2534dc84dba1ca12ccfb69b02907e0afb6c1e01b
황준현 <ihrug000@gmail.com>
2022-12-02 01:44:45 +0900

    securityjwtlogin init~mysql

---
+++ jwtlogin/src/test/java/com/jwt/controller/JwtloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
